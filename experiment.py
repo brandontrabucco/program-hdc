79ba45dc87edc4192a4ee4d80c271aeb41cfe7c9
Brandon Trabucco <brandontrabucco@gmail.com>
2017-12-21 00:57:08 -0800

    added hypercomplex algebra

--- experiment.py
+++ experiment.py
@@ -9,10 +9,10 @@ PLOT_BASEDIR = (PROJECT_BASEDIR + "Results/")
 CHECKPOINT_BASEDIR = (PROJECT_BASEDIR + "Backups/")
 
 
-SEQUENCE_LENGTH = 50
-MEMORY_LOCATIONS = 1000
-MEMORY_DEPTH = 10
-HIDDEN_SIZE = 100
+SEQUENCE_LENGTH = 10
+MEMORY_LOCATIONS = 10000
+MEMORY_DEPTH = 4
+HIDDEN_SIZE = 200
 INITIAL_LEARNING_RATE = 0.0001
 DECAY_STEPS = SEQUENCE_LENGTH
 DECAY_FACTOR = 1.0
@@ -35,6 +35,185 @@ EXTENSION_RESULT = "_result"
 
 COLLECTION_LOSSES = "_losses"
 COLLECTION_PARAMETERS = "_params"
+
+
+def hypercomplex_conjugate(a, n=MEMORY_DEPTH):
+    if n == 1:
+        a_conjugate = tf.concat([
+            tf.strided_slice(
+                a, 
+                [0, 0], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH])], 1)
+        return a_conjugate
+
+    if n == 2:
+        a_conjugate = tf.concat([
+            tf.strided_slice(
+                a, 
+                [0, 0], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            -1 * tf.strided_slice(
+                a, 
+                [0, 1], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH])], 1)
+        return a_conjugate
+    
+    if n == 4:
+        a_conjugate = tf.concat([
+            tf.strided_slice(
+                a, 
+                [0, 0], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            -1 * tf.strided_slice(
+                a, 
+                [0, 1], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            -1 * tf.strided_slice(
+                a, 
+                [0, 2], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            -1 * tf.strided_slice(
+                a, 
+                [0, 3], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH])], 1)
+        return a_conjugate
+
+
+def hypercomplex_multiply(a, b, n=MEMORY_DEPTH):
+    if n == 1:
+        b_shifted_0 = tf.concat([
+            tf.strided_slice(
+                b, 
+                [0, 0], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH])], 1)
+        return tf.concat([
+            tf.reduce_sum((a * b_shifted_0), axis=1, keep_dims=True)], axis=1)
+    
+    if n == 2:
+        b_shifted_0 = tf.concat([
+            tf.strided_slice(
+                b, 
+                [0, 0], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            -1 * tf.strided_slice(
+                b, 
+                [0, 1], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH])], 1)
+        b_shifted_1 = tf.concat([
+            tf.strided_slice(
+                b, 
+                [0, 1], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            tf.strided_slice(
+                b, 
+                [0, 0], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH])], 1)
+        return tf.concat([
+            tf.reduce_sum((a * b_shifted_0), axis=1, keep_dims=True),
+            tf.reduce_sum((a * b_shifted_1), axis=1, keep_dims=True)], axis=1)
+    
+    elif n == 4:
+        b_shifted_0 = tf.concat([
+            tf.strided_slice(
+                b, 
+                [0, 0], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            -1 * tf.strided_slice(
+                b, 
+                [0, 1], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            -1 * tf.strided_slice(
+                b, 
+                [0, 2], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            -1 * tf.strided_slice(
+                b, 
+                [0, 3], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH])], 1)
+        b_shifted_1 = tf.concat([
+            tf.strided_slice(
+                b, 
+                [0, 1], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            tf.strided_slice(
+                b, 
+                [0, 0], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            -1 * tf.strided_slice(
+                b, 
+                [0, 3], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            tf.strided_slice(
+                b, 
+                [0, 2], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH])], 1)
+        b_shifted_2 = tf.concat([
+            tf.strided_slice(
+                b, 
+                [0, 2], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            tf.strided_slice(
+                b, 
+                [0, 3], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            tf.strided_slice(
+                b, 
+                [0, 0], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            -1 * tf.strided_slice(
+                b, 
+                [0, 1], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH])], 1)
+        b_shifted_3 = tf.concat([
+            tf.strided_slice(
+                b, 
+                [0, 3], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            -1 * tf.strided_slice(
+                b, 
+                [0, 2], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            tf.strided_slice(
+                b, 
+                [0, 1], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH]), 
+            -1 * tf.strided_slice(
+                b, 
+                [0, 0], 
+                [MEMORY_LOCATIONS, MEMORY_DEPTH], 
+                strides=[1, MEMORY_DEPTH])], 1)
+        return tf.concat([
+            tf.reduce_sum((a * b_shifted_0), axis=1, keep_dims=True),
+            tf.reduce_sum((a * b_shifted_1), axis=1, keep_dims=True),
+            tf.reduce_sum((a * b_shifted_2), axis=1, keep_dims=True),
+            tf.reduce_sum((a * b_shifted_3), axis=1, keep_dims=True)], axis=1)
 
 
 def initialize_weights_cpu(name, shape, standard_deviation=0.01, decay_factor=None, collection=None):
@@ -143,20 +322,29 @@ def controller(data_in):
 
         linear_w_content = initialize_weights_cpu(
             (scope.name + EXTENSION_CONTENT), 
-            [HIDDEN_SIZE, 1, MEMORY_LOCATIONS])
+            [HIDDEN_SIZE, MEMORY_DEPTH, MEMORY_LOCATIONS])
         linear_b_content = initialize_biases_cpu(
             (scope.name + EXTENSION_CONTENT), 
-            [MEMORY_LOCATIONS, 1, 1])
+            [MEMORY_LOCATIONS, MEMORY_DEPTH, 1])
         content = tf.add(tf.tensordot(tf.transpose(linear_w_content), activation, 1), linear_b_content)
 
 
-        linear_w_query = initialize_weights_cpu(
-            (scope.name + EXTENSION_QUERY), 
+        linear_w_query_l = initialize_weights_cpu(
+            (scope.name + EXTENSION_QUERY  + "_l"), 
             [HIDDEN_SIZE, MEMORY_DEPTH, MEMORY_LOCATIONS])
-        linear_b_query = initialize_biases_cpu(
-            (scope.name + EXTENSION_QUERY), 
+        linear_b_query_l = initialize_biases_cpu(
+            (scope.name + EXTENSION_QUERY  + "_l"), 
             [MEMORY_LOCATIONS, MEMORY_DEPTH, 1])
-        query = tf.add(tf.tensordot(tf.transpose(linear_w_query), activation, 1), linear_b_query)
+        query_l = tf.add(tf.tensordot(tf.transpose(linear_w_query_l), activation, 1), linear_b_query_l)
+
+
+        linear_w_query_c = initialize_weights_cpu(
+            (scope.name + EXTENSION_QUERY  + "_c"), 
+            [HIDDEN_SIZE, MEMORY_DEPTH, MEMORY_LOCATIONS])
+        linear_b_query_c = initialize_biases_cpu(
+            (scope.name + EXTENSION_QUERY  + "_c"), 
+            [MEMORY_LOCATIONS, MEMORY_DEPTH, 1])
+        query_c = tf.add(tf.tensordot(tf.transpose(linear_w_query_c), activation, 1), linear_b_query_c)
 
 
         linear_w_output = initialize_weights_cpu(
@@ -175,10 +363,10 @@ def controller(data_in):
 
 
     CONTROLLER_INITIALIZED = True
-    return location, content, query, output
+    return location, content, query_l, query_c, output
 
 
-def update_memory(key_batch, value_batch, query_batch):
+def update_memory(key_batch, value_batch, query_l_batch, query_c_batch):
 
     global MEMORY_INITIALIZED
 
@@ -190,8 +378,13 @@ def update_memory(key_batch, value_batch, query_batch):
             [MEMORY_LOCATIONS, MEMORY_DEPTH, 1])
 
         
-        memory = memory + key_batch * tf.tile(value_batch, [1, MEMORY_DEPTH, 1])
-        output = tf.reduce_sum(memory * query_batch, axis=1)
+        memory = memory + hypercomplex_multiply(key_batch, value_batch)
+        output = tf.reduce_sum(
+            hypercomplex_multiply(
+                hypercomplex_conjugate(query_l_batch), 
+                hypercomplex_multiply(
+                    memory, 
+                    hypercomplex_conjugate(query_c_batch))), axis=1)
 
     MEMORY_INITIALIZED = True
     return output
@@ -279,7 +472,7 @@ def train_model(num_epochs=1):
                 strides=[SEQUENCE_LENGTH, 1]))
 
 
-            location, content, query, prediction = controller(
+            location, content, query_l, query_c, prediction = controller(
                 tf.concat([sliced_input, feedback], 0))
             predicted_sequence = tf.concat([predicted_sequence, prediction], 1)
 
@@ -287,7 +480,7 @@ def train_model(num_epochs=1):
                 sliced_output, 
                 prediction,
                 (PREFIX_CONTROLLER + COLLECTION_LOSSES))
-            feedback = update_memory(location, content, query)
+            feedback = update_memory(location, content, query_l, query_c)
 
 
         prediced_indices = tf.argmax(predicted_sequence, axis=0)
